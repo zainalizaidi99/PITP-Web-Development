0fa870927f09b9ba6791304130b0bb88eab6e98a
zainalizaidi99 <2005zainaz@gmail.com>
2024-10-22 04:47:02 -0800

    Delete JAVASCRIPT directory

--- JAVASCRIPT/code1.js
+++
@@ -1,8 +0,0 @@
-let string = "245";
-let integer = 10;
-
-let exchange = Number(string);
-console.log( typeof exchange); 
-
-let exchange1 = String(integer)
-console.log(typeof exchange1); 
--- JAVASCRIPT/variable.js
+++
@@ -1,6 +0,0 @@
-let Name = "Zain Ali";
-const rollNo = 215;
-let section = "SEC-D";
-const seatNo = 45;
-
-console.table({Name,rollNo,section,seatNo});  

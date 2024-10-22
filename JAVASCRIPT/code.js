74f4bb47c734c16e2ed22cb8d3c3ee8d1329e8cc
zainalizaidi99 <2005zainaz@gmail.com>
2024-10-22 04:46:46 -0800

    Add files via upload

---
+++ JAVASCRIPT/code.js
@@ -0,0 +1,6 @@
+const accountId = 134;
+let accountEmail = "safee";
+var accountPassword = "123456";
+accountCity = "Karachi";
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
---
+++ JAVASCRIPT/variable.js
@@ -0,0 +1,6 @@
+let Name = "Zain Ali";
+const rollNo = 215;
+let section = "SEC-D";
+const seatNo = 45;
+
+console.table({Name,rollNo,section,seatNo});  
